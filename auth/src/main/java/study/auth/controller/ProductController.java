313f584900e7c26b4cf40113a5a3fc3e2bc74fef
sall6550 <keinpark1@naver.com>
2025-02-24 02:02:32 +0900

    feat: 스프링 시큐리티 로그인 구현

--- auth/src/main/java/study/auth/controller/ProductController.java
+++ auth/src/main/java/study/auth/controller/ProductController.java
@@ -1,20 +1,23 @@
 package study.auth.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import study.auth.entity.User;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import study.auth.security.UserDetailsImpl;
 
 @Controller
 @RequestMapping("/api")
 public class ProductController {
 
     @GetMapping("/products")
-    public String getProducts(HttpServletRequest req) {
-        System.out.println("ProductController.getProducts : 인증 완료");
-        User user = (User) req.getAttribute("user");
+    public String getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        User user = userDetails.getUser();
         System.out.println("user.getUsername() = " + user.getUsername());
+        System.out.println("user.getEmail() = " + user.getEmail());
 
         return "redirect:/";
     }
