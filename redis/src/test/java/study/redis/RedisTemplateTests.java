4d4247312d1510e8a4a6fcfc58ae3bb3d1852e4e
sall6550 <keinpark1@naver.com>
2025-03-11 04:09:23 +0900

    feat: StringRedisTemplate을 이용한 테스트

---
+++ redis/src/test/java/study/redis/RedisTemplateTests.java
@@ -0,0 +1,43 @@
+package study.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest
+public class RedisTemplateTests {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    public void stringValueOpsTest() {
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        ops.set("simplekey", "simplevalue");
+        System.out.println(ops.get("simplekey"));
+        ops.set("greeting", "hello redis!");
+        System.out.println(ops.get("greeting"));
+    }
+
+    @Test
+    public void stringSetOpsTest() {
+        SetOperations<String, String> setOps = stringRedisTemplate.opsForSet();
+        setOps.add("hobbies", "games");
+        setOps.add("hobbies", "coding");
+        setOps.add("hobbies", "alcohol");
+        setOps.add("hobbies", "games");
+        System.out.println(setOps.size("hobbies"));
+    }
+
+    @Test
+    public void redisOpsTest() {
+        stringRedisTemplate.expire("simplekey", 5, TimeUnit.SECONDS);
+        stringRedisTemplate.expire("greeting", 10, TimeUnit.SECONDS);
+        stringRedisTemplate.expire("hobbies", 15, TimeUnit.SECONDS);
+    }
+}
